ae9a29e35ddc74d26a5c90349d60af660882dc3f
IchsanJulyanto <57021746+IchsanJulyanto@users.noreply.github.com>
2019-11-16 00:50:39 -0800

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,24 @@
+function myBiodata(biodata){
+  let biodataObj = {
+    "name": 'Ichsan Julyanto',
+    "address": 'Jl.Gajah Mada',
+    "hobbies": [
+      'Coding', 'Listening to music', 'Draw a picture'
+      ],
+    "is_married": false,
+    "school": {
+      "highSchool": 'SMK N 2 BAU-BAU',
+      "university": 'Null'
+    },
+    "skill": [
+      {
+        "HTML5": "Beginner",
+        "CSS": "Beginner",
+        "Javascript": "Beginner",
+      }
+    ]
+  }
+
+  return biodataObj
+}
+console.log(myBiodata('Ichsan');
